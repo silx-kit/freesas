6ed7737338e57968a29bc97b9b4c12060f113a09
Guillaume Bonamis <guillaume.bonamis@esrf.fr>
2015-06-12 09:32:06 +0200

    sript to align two models

--- scripts/supcomb.py
+++ scripts/supcomb.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-from __builtin__ import False
 __author__ = "Guillaume Bonamis"
 __license__ = "MIT"
 __copyright__ = "2015, ESRF"
@@ -9,12 +8,22 @@ from os.path import dirname, abspath
 base = dirname(dirname(abspath(__file__)))
 from freesas.align import alignment_2models
 
-parser = argparse.ArgumentParser(description="align several models and calculate NSD")
-parser.add_argument("file1", help="pdb file")
-parser.add_argument("file2", help="second pdb file")
-parser.add_argument("-f", "--fast", action="store_false", help="using fast mode, default=slow")
-parser.add_argument("-e", "--enantiomorphs", action="store_false", help="do not search enantiomorphs, default=search")
+usage = "./supcomb file1.pdb file2.pdb [OPTIONS]"
+description = "align several models and calculate NSD"
+parser = argparse.ArgumentParser(usage=usage, description=description)
+parser.add_argument("file", metavar="FILE", nargs='+', help="pdb files to align")
+parser.add_argument("-m", "--mode",dest="mode", type=str, choices=["SLOW", "FAST"], default="SLOW", help="Either SLOW or FAST, default: %(default)s)")
+parser.add_argument("-e", "--enantiomorphs",type=str, choices=["YES", "NO"], default="YES", help="Search enantiomorphs, YES or NO, default: %(default)s)")
 
 args = parser.parse_args()
-dist = alignment_2models(args.file1, args.file2, enantiomorphs=args.enantiomorphs, slow=args.fast)
+if args.mode=="SLOW":
+    slow = True
+else:
+    slow = False
+if args.enantiomorphs=="YES":
+    enantiomorphs = True
+else:
+    enantiomorphs = False
+dist = alignment_2models(args.file[0], args.file[1], enantiomorphs=enantiomorphs, slow=slow)
+print "%s and %s aligned"%(args.file[0], args.file[1])
 print "NSD after optimized alignment = %s"%(dist)
