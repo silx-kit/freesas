722c9e2ad752a169acd27b51f978dc79e27534ac
Guillaume Bonamis <guillaume.bonamis@esrf.fr>
2015-06-26 11:30:30 +0200

    change the way to make the grid, using an other symmetry

--- freesas/average.py
+++ freesas/average.py
@@ -5,6 +5,63 @@ __copyright__ = "2015, ESRF"
 import numpy
 from freesas.model import SASModel
 
+class Grid():
+    def __init__(self, nbknots=None):
+        self.inputs = []
+        self.size = []
+        self.nbknots = nbknots if nbknots is not None else 5000
+        self.radius = None
+        self.coordknots = []
+
+    def __repr__(self):
+        return "Grid with %i knots"%self.knots.shape[0]
+
+    def spatial_extent(self):
+        """
+        Calculate the maximal extent of input models
+        
+        @return self.size: 6-list with x,y,z max and then x,y,z min
+        """
+        atoms = []
+        for files in self.inputs:
+            m = SASModel()
+            m.read(files)
+            if len(atoms)==0:
+                atoms = m.atoms
+            else:
+                atoms = numpy.append(atoms, m.atoms, axis=0)
+        
+        coordmin = atoms.min(axis=0)
+        coordmax = atoms.max(axis=0)
+        self.size = [coordmax[0],coordmax[1],coordmax[2],coordmin[0],coordmin[1],coordmin[2]]
+        
+        return self.size
+
+    def calc_radius(self):
+        """
+        Calculate the radius of each point of a hexagonal close-packed grid, 
+        knowing the total volume and the number of knots in this grid.
+        
+        @return radius: the radius of each knot of the grid
+        """
+        if len(self.size)==0:
+            self.spatial_extent()
+        size = self.size
+        dx = size[0] - size[3]
+        dy = size[1] - size[4]
+        dz = size[2] - size[5]
+        volume = dx*dy*dz
+        
+        density = numpy.pi/(3*2**0.5)
+        radius = ((3/(4*numpy.pi))*density*volume/self.nbknots)**(1.0/3)
+        self.radius = radius
+        
+        return radius
+
+    def make_grid(self):
+        """
+        """
+
 class AverModels():
     def __init__(self, filename=None, reference=None):
         self.inputfiles = []
@@ -33,91 +90,6 @@ class AverModels():
             else:
                 self.atoms = numpy.append(self.atoms, m.atoms, axis=0)
         return self.atoms
-
-    def calc_radius(self):
-        """
-        Select the fineness of the reference model and set this value as 2 times self.radius
-        
-        @return self.radius: radius of an atom in the new grid
-        """
-        reference = SASModel()
-        reference.read(self.inputfiles[self.reference])
-        reference._calc_fineness()
-        self.radius = 0.5*reference.fineness
-        
-        return self.radius
-
-    def gridsize(self):
-        """
-        Calculate the maximal area occupied by models.
-        
-        @return self.size: 6-list with coordinates x,y,z max and x,y,z min
-        """
-        if len(self.atoms)==0:
-            self.atoms = self.models_pooling()
-        atoms = self.atoms
-        radius = self.radius
-        
-        coordmin = atoms.min(axis=0) - 3*radius
-        coordmax = atoms.max(axis=0) + 3*radius
-        self.size = [coordmax[0],coordmax[1],coordmax[2],coordmin[0],coordmin[1],coordmin[2]]
-        
-        return self.size
-
-    def makegrid(self):
-        """
-        Create the new grid points using the area occupied by models.
-        
-        @return grid: 2d array, coordinates of each point of the grid, fourth column for the occupancy.
-        """
-        if not self.radius:
-            self.calc_radius()
-        if not self.size:
-            self.size = self.gridsize()
-        size = self.size
-        radius = self.radius
-        
-        x = 0.0
-        y = 0.0
-        z = 0.0
-        xlist = []
-        ylist = []
-        zlist = []
-        while x<=size[0]:
-            xlist.append(x)
-            x += 2*radius
-        while y<=size[1]:
-            ylist.append(y)
-            y += 2*radius
-        while z<=size[2]:
-            zlist.append(z)
-            z += 2*radius
-        
-        x = - 2*radius
-        y = - 2*radius
-        z = - 2*radius
-        while x>=size[3]:
-            xlist.append(x)
-            x -= 2*radius
-        while y>=size[4]:
-            ylist.append(y)
-            y -= 2*radius
-        while z>=size[5]:
-            zlist.append(z)
-            z -= 2*radius
-        knots = len(xlist)*len(ylist)*len(zlist)
-        
-        for i in range(len(xlist)):
-            for j in range(len(ylist)):
-                for k in range(len(zlist)):
-                    if i==j==k==0:
-                        grid = numpy.array([[xlist[i], ylist[j], zlist[k], 0.0]], dtype="float")
-                    else:
-                        grid = numpy.append(grid, [[xlist[i], ylist[j], zlist[k], 0.0]], axis=0)
-        if grid.shape[0] != knots:
-            print "pb with grid lenght"
-        self.grid = grid
-        return grid
 
     def trilin_interp(self, atom, gridpoint):
         """
@@ -192,39 +164,26 @@ class AverModels():
         self.grid = sortedgrid
         return sortedgrid
 
-    def makeheader(self):
-        """
-        """
-        header = self.header
-        
-        header.append("number of models averaged : %s"%len(self.inputfiles))
-        header.append("filenames :")
-        for i in self.inputfiles:
-            header.append("--- %s"%i)
-        header.append("total number of atoms : %s \n"%self.atoms.shape[0])
-        
-        for i in range(self.grid.shape[0]):
-            x = round(self.grid[i, 0], 3)
-            y = round(self.grid[i, 1], 3)
-            z = round(self.grid[i, 2], 3)
-            occ = round(self.grid[i, 3], 2)
-            header.append("ATOM      1  CA  ASP    1       %s  %s  %s  %s 20.0 0 2 201    "%(x, y, z, occ))
-            
-        for i in range(len(header)):
-            print header[i]
-        
-
 if __name__ == "__main__":
-    aver = AverModels()
-    aver.inputfiles = ["aligned-11.pdb"]
-    aver.models_pooling()
-    aver.calc_radius()
-    print "%s atoms"%aver.atoms.shape[0]
-    print "radius = %s"%aver.radius
-    aver.gridsize()
-    print "grid size : ", aver.size
-    aver.makegrid()
-    print "%s points in the grid"%aver.grid.shape[0]
-    aver.assign_occupancy()
-    print aver.grid
+    n = SASModel()
+    n.read("aligned-11.pdb")
+    n._calc_fineness()
+    print "fineness aligned-11.pdb = ", n.fineness
+    
+    m = SASModel()
+    m.read("damaver.pdb")
+    m._calc_fineness()
+    print "fineness damaver.pdb = ", m.fineness
+    
+    p = SASModel()
+    p.read("damfilt.pdb")
+    p._calc_fineness()
+    print "fineness damfilt.pdb = ", p.fineness
+    
+    q = SASModel()
+    q.read("damstart.pdb")
+    q._calc_fineness()
+    print "fineness damstart.pdb = ", q.fineness
+    
+            
     print "DONE"
