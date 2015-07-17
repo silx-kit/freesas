60fb701c4282b220a4781994f787b373f5a23513
Guillaume Bonamis <guillaume.bonamis@esrf.fr>
2015-07-17 14:15:30 +0200

    Documentation for Freesas... at the moment

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,6 +32,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.pngmath',
+    'sphinxcontrib.programoutput'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
