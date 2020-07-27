4bf57cc1be2ee31ed089c4aa2835bdf10a681b04
Martha Brennich <martha.brennich@googlemail.com>
2020-07-27 17:10:05 +0200

    Added licence information for mock_open...

--- freesas/test/mock_open_38.py
+++ freesas/test/mock_open_38.py
@@ -1,6 +1,10 @@
 """
 This is the Python 3.8 implementation of mock_open taken from
 https://github.com/python/cpython/blob/3.8/Lib/unittest/mock.py
+Hence: 
+"Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 Python Software Foundation;
+All Rights Reserved"
 """
 
 import io
