79b3f77751e45e2cad93d3fe90cb2eeff0193061
Martha Brennich <martha.brennich@googlemail.com>
2020-11-11 15:56:34 +0100

    Added exit code to e2e runner

--- e2etest/e2etest.py
+++ e2etest/e2etest.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""Run the end to end tests of the project."""
+
 __author__ = "Martha Brennich"
 __license__ = "MIT"
 __copyright__ = "2020"
 __date__ = "11/07/2020"
 
+import sys
 import unittest
 import e2etest_freesas
 
 
-
 def suite():
-    testSuite = unittest.TestSuite()
-    testSuite.addTest(e2etest_freesas.suite())
-    return testSuite
+    """Creates suite for e2e tests"""
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(e2etest_freesas.suite())
+    return test_suite
 
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
-    runner.run(suite())
+    result = runner.run(suite())
+
+    if result.wasSuccessful():
+        EXIT_STATUS = 0
+    else:
+        EXIT_STATUS = 1
+
+    sys.exit(EXIT_STATUS)
