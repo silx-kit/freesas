ee1ed4b9e6c6af756d375021a5d0a3780ccac5fc
Martha <martha.brennich@googlemail.com>
2021-01-30 11:28:37 +0100

    Some cleaning

--- e2etest/e2etest_guinier_apps.py
+++ e2etest/e2etest_guinier_apps.py
@@ -35,9 +35,7 @@ class TestFreeSAS(unittest.TestCase):
             stderr=STDOUT,
             check=True,
         )
-        self.assertEqual(
-            run_app.returncode, 0, msg=f"{app_name} completed well"
-        )
+        self.assertEqual(run_app.returncode, 0, msg=f"{app_name} completed well")
         run_app_output = str(run_app.stdout, "utf-8")[:-1]
         self.assertFalse(
             linesep in run_app_output,
@@ -47,12 +45,8 @@ class TestFreeSAS(unittest.TestCase):
             str(self.bsa_filename) in run_app_output,
             msg="filename of testdata found in output of {app_name}",
         )
-        self.assertTrue(
-            "I₀" in run_app_output, msg=f"I₀ in {app_name}"
-        )
-        self.assertTrue(
-            "Rg" in run_app_output, msg=f"Rg in {app_name}"
-        )
+        self.assertTrue("I₀" in run_app_output, msg=f"I₀ in {app_name}")
+        self.assertTrue("Rg" in run_app_output, msg=f"Rg in {app_name}")
 
     def test_two_easy_files_without_arguments(self):
         """
@@ -95,6 +89,7 @@ class TestFreeSAS(unittest.TestCase):
             self.assertTrue("Rg" in result, f"Rg in {app_name} output")
 
     def test_csv_output_with_two_files(self):
+        """Test wether the about in csv format is consistent"""
         app_name: str = self.extra_arg["app"]
         test_output_name = pathlib.Path(self.cwd, f"{app_name}.csv")
         try:
@@ -122,62 +117,128 @@ class TestFreeSAS(unittest.TestCase):
         )
         self.assertEqual(
             run_app.stdout,
-            b'',
+            b"",
             msg=f"{app_name} for 2 files to csv provided no output to stdout",
         )
-        with open(test_output_name, 'r') as test_output_file:
+        with open(test_output_name, "r") as test_output_file:
             test_output_header = test_output_file.readline()[:-1].split(",")
             test_output_result = test_output_file.readlines()
         test_output_result = [line[:-1].split(",") for line in test_output_result]
-        expected_header = {'File', 'Rg', 'Rg StDev', 'I(0)', 'I(0) StDev', 'First point', 'Last point', 'Quality', 'Aggregated'}
-        self.assertEqual(expected_header^set(test_output_header), set(), msg=f"header created by {app_name} contains the expected element")
-        self.assertEqual(len(test_output_header), len(set(test_output_header)), msg=f"header created by {app_name} contains no duplicates")
+        expected_header = {
+            "File",
+            "Rg",
+            "Rg StDev",
+            "I(0)",
+            "I(0) StDev",
+            "First point",
+            "Last point",
+            "Quality",
+            "Aggregated",
+        }
+        self.assertEqual(
+            expected_header ^ set(test_output_header),
+            set(),
+            msg=f"header created by {app_name} contains the expected element",
+        )
+        self.assertEqual(
+            len(test_output_header),
+            len(set(test_output_header)),
+            msg=f"header created by {app_name} contains no duplicates",
+        )
+        index = {}
+        for header_item in expected_header:
+            index[header_item] = test_output_header.index(header_item)
 
-        file_name_index = test_output_header.index('File')
-        Rg_index = test_output_header.index('Rg')
-        Rg_std_index = test_output_header.index('Rg StDev')
-        I0_index = test_output_header.index('I(0)')
-        I0_std_index = test_output_header.index('I(0) StDev')
-        first_point_index = test_output_header.index('First point')
-        last_point_index = test_output_header.index('Last point')
-        quality_index = test_output_header.index('Quality')
-        aggregated_index = test_output_header.index('Aggregated')
-
-        self.assertEqual(len(test_output_result), 2, msg=f"result file created by {app_name} contains 2 results")
-        self.assertEqual({str(self.bsa_filename),str(self.sas_curve2_filename)}, {test_output_result[0][file_name_index], test_output_result[1][file_name_index]}, msg=f"result file created by {app_name} contains results for the right files" )
-        for result in  test_output_result:
-            if result[file_name_index] == str(self.bsa_filename):
-                self.assertTrue(abs(float(result[Rg_index]) - 2.95) <= 0.1, msg=f"Rg for BSA by {app_name} between 2.85 and 3.05")
-                self.assertTrue(float(result[Rg_std_index])  < 0.3, msg=f"Rg std. for BSA by {app_name} below 0.3")
-                self.assertTrue(abs(float(result[I0_index]) - 62.5) <= 2.5, msg=f"I0 for BSA by {app_name} between 60 and 65")
-                self.assertTrue(float(result[I0_std_index])  < 5, msg=f"I0 std. for BSA by {app_name} below 5")
-                self.assertTrue(float(result[first_point_index]) < 70, msg=f"Guinier region start for BSA by {app_name} below 70")
-                self.assertTrue(float(result[last_point_index])  > 85 , msg=f"Guinier region end for BSA by {app_name} above 85")
-                self.assertTrue(float(result[quality_index]) > 0.8, msg=f"Fit quality BSA by {app_name} above 0.8")
-                self.assertTrue(float(result[aggregated_index]) < 0.05, msg=f"Aggregation number for BSA by {app_name} below 0.05")
-            elif result[file_name_index] == str(self.sas_curve2_filename):
-                self.assertTrue(abs(float(result[Rg_index]) - 3.05) <= 0.2, msg=f"Rg for SASDF52 by {app_name} between 2.85 and 3.25")
-                self.assertTrue(float(result[Rg_std_index])  < 0.15, msg=f"Rg std. for SASDF52 by {app_name} below 0.5")
-                self.assertTrue(abs(float(result[I0_index]) - 64.5) <= 2.5, msg=f"I0 for SASDF52 by {app_name} between 62 and 67")
-                self.assertTrue(float(result[I0_std_index])  < 5, msg=f"I0 std. for SASDF52 by {app_name} below 5")
-                self.assertTrue(float(result[first_point_index])  < 35, msg=f"Guinier region start for SASDF52 by {app_name} below 35")
-                self.assertTrue(float(result[last_point_index]) > 80, msg=f"Guinier region end for SASDF52 by {app_name} above 80")
-                self.assertTrue(float(result[quality_index]) > 0.8, msg=f"Fit quality SASDF52 by {app_name} above 0.8")
-                self.assertTrue(float(result[aggregated_index] ) < 0.1, msg=f"Aggregation number for SASDF52 by {app_name} below 0.1")
+        self.assertEqual(
+            len(test_output_result),
+            2,
+            msg=f"result file created by {app_name} contains 2 results",
+        )
+        self.assertEqual(
+            {str(self.bsa_filename), str(self.sas_curve2_filename)},
+            {
+                test_output_result[0][index["File"]],
+                test_output_result[1][index["File"]],
+            },
+            msg=f"result file created by {app_name} contains results for the right files",
+        )
+        for result in test_output_result:
+            if result[index["File"]] == str(self.bsa_filename):
+                self.assertTrue(
+                    abs(float(result[index["Rg"]]) - 2.95) <= 0.1,
+                    msg=f"Rg for BSA by {app_name} between 2.85 and 3.05",
+                )
+                self.assertTrue(
+                    float(result[index["Rg StDev"]]) < 0.3,
+                    msg=f"Rg std. for BSA by {app_name} below 0.3",
+                )
+                self.assertTrue(
+                    abs(float(result[index["I(0)"]]) - 62.5) <= 2.5,
+                    msg=f"I0 for BSA by {app_name} between 60 and 65",
+                )
+                self.assertTrue(
+                    float(result[index["I(0) StDev"]]) < 5,
+                    msg=f"I0 std. for BSA by {app_name} below 5",
+                )
+                self.assertTrue(
+                    float(result[index["First point"]]) < 70,
+                    msg=f"Guinier region start for BSA by {app_name} below 70",
+                )
+                self.assertTrue(
+                    float(result[index["Last point"]]) > 85,
+                    msg=f"Guinier region end for BSA by {app_name} above 85",
+                )
+                self.assertTrue(
+                    float(result[index["Quality"]]) > 0.8,
+                    msg=f"Fit quality BSA by {app_name} above 0.8",
+                )
+                self.assertTrue(
+                    float(result[index["Aggregated"]]) < 0.05,
+                    msg=f"Aggregation number for BSA by {app_name} below 0.05",
+                )
+            elif result[index["File"]] == str(self.sas_curve2_filename):
+                self.assertTrue(
+                    abs(float(result[index["Rg"]]) - 3.05) <= 0.2,
+                    msg=f"Rg for BSA by {app_name} between 2.85 and 3.25",
+                )
+                self.assertTrue(
+                    float(result[index["Rg StDev"]]) < 0.15,
+                    msg=f"Rg std. for SASDF52 by {app_name} below 0.5",
+                )
+                self.assertTrue(
+                    abs(float(result[index["I(0)"]]) - 64.5) <= 2.5,
+                    msg=f"I0 for SASDF52 by {app_name} between 62 and 67",
+                )
+                self.assertTrue(
+                    float(result[index["I(0) StDev"]]) < 5,
+                    msg=f"I0 std. for SASDF52 by {app_name} below 5",
+                )
+                self.assertTrue(
+                    float(result[index["First point"]]) < 35,
+                    msg=f"Guinier region start for SASDF52 by {app_name} below 35",
+                )
+                self.assertTrue(
+                    float(result[index["Last point"]]) > 80,
+                    msg=f"Guinier region end for SASDF52 by {app_name} above 80",
+                )
+                self.assertTrue(
+                    float(result[index["Quality"]]) > 0.8,
+                    msg=f"Fit quality SASDF52 by {app_name} above 0.8",
+                )
+                self.assertTrue(
+                    float(result[index["Aggregated"]]) < 0.1,
+                    msg=f"Aggregation number for SASDF52 by {app_name} below 0.1",
+                )
 
 
 def suite():
     test_suite = unittest.TestSuite()
     for app in ["auto_gpa.py", "auto_guinier.py", "autorg.py"]:
-        test_suite.addTest(
-            TestFreeSAS("test_one_bm29_bsa_without_arguments", app=app)
-        )
+        test_suite.addTest(TestFreeSAS("test_one_bm29_bsa_without_arguments", app=app))
         test_suite.addTest(
             TestFreeSAS("test_two_easy_files_without_arguments", app=app)
         )
-        test_suite.addTest(
-            TestFreeSAS("test_csv_output_with_two_files", app=app)
-        )
+        test_suite.addTest(TestFreeSAS("test_csv_output_with_two_files", app=app))
     return test_suite
 
 
