6fad74b1f2b0d4347d0619db095549393a9c2821
Martha <martha.brennich@googlemail.com>
2021-05-07 17:43:05 +0200

    Revert shell script change again

--- build-deb.sh
+++ build-deb.sh
@@ -39,7 +39,7 @@ deb_name=$(echo "$source_project" | tr '[:upper:]' '[:lower:]')
 if [ -f /etc/debian_version ]
 then 
     debian_version=$(< /etc/debian_version cut -d. -f1 | grep -o '[0-9]*')
-    if [ -z "$debian_version" ]
+    if [ -z $debian_version ]
     then
     #we are probably on a ubuntu platform
         debian_version=$(< /etc/debian_version  cut -d/ -f1)
@@ -70,8 +70,8 @@ else
 fi
 target_system=debian${debian_version}
 
-project_directory="$(dirname) \"$(0)\""
-project_directory="( cd \"$(project_directory)\" && pwd )" # absolutized
+project_directory="`dirname \"$0\"`"
+project_directory="`( cd \"$project_directory\" && pwd )`" # absolutized
 dist_directory=${project_directory}/dist/${target_system}
 build_directory=${project_directory}/build/${target_system}
 
@@ -81,12 +81,9 @@ then
 fi
 
 usage="usage: $(basename "$0") [options]
-
 Build the Debian ${debian_version} package of the ${project} library.
-
 If the build succeed the directory dist/debian${debian_version} will
 contains the packages.
-
 optional arguments:
     --help          Show this help text
     --install       Install the packages generated at the end of
@@ -125,23 +122,23 @@ do
           ;;
       --debian9)
           debian_version=9
-          target_system="debian${debian_version}"
-          dist_directory="${project_directory}/dist/${target_system}"
-          build_directory="${project_directory}/build/${target_system}"
+          target_system=debian${debian_version}
+          dist_directory=${project_directory}/dist/${target_system}
+          build_directory=${project_directory}/build/${target_system}
           shift
           ;;
       --debian10)
           debian_version=10
-          target_system="debian${debian_version}"
-          dist_directory="${project_directory}/dist/${target_system}"
-          build_directory="${project_directory}/build/${target_system}"
+          target_system=debian${debian_version}
+          dist_directory=${project_directory}/dist/${target_system}
+          build_directory=${project_directory}/build/${target_system}
           shift
           ;;
       --debian11)
           debian_version=11
-          target_system="debian${debian_version}"
-          dist_directory="${project_directory}/dist/${target_system}"
-          build_directory="${project_directory}/build/${target_system}"
+          target_system=debian${debian_version}
+          dist_directory=${project_directory}/dist/${target_system}
+          build_directory=${project_directory}/build/${target_system}
           shift
           ;;
       -*)
@@ -159,58 +156,58 @@ clean_up()
 {
     echo "Clean working dir:"
     # clean up previous build
-    rm -rf "${build_directory}"
+    rm -rf ${build_directory}
     # create the build context
-    mkdir -p "${build_directory}"
+    mkdir -p ${build_directory}
 }
 
 build_deb() {
-    tarname="${project}_${debianversion}.orig.tar.gz"
+    tarname=${project}_${debianversion}.orig.tar.gz
     clean_up
     python3 setup.py debian_src
-    cp -f "dist/${tarname} ${build_directory}"
-    if [ -f "dist/${project}-testimages.tar.gz" ]
+    cp -f dist/${tarname} ${build_directory}
+    if [ -f dist/${project}-testimages.tar.gz ]
     then
-      cp -f "dist/${project}-testimages.tar.gz ${build_directory}"
+      cp -f dist/${project}-testimages.tar.gz ${build_directory}
     fi
     
-    cd "${build_directory}"
-    tar -xzf "${tarname}"
+    cd ${build_directory}
+    tar -xzf ${tarname}
     
-    directory="${project}"-"${strictversion}"
-    newname="${deb_name}_${debianversion}.orig.tar.gz"
+    directory=${project}-${strictversion}
+    newname=${deb_name}_${debianversion}.orig.tar.gz
     
     #echo tarname $tarname newname $newname
-    if [ "$tarname" != "$newname" ]
+    if [ $tarname != $newname ]
     then
-      if [ -h "$newname" ]
+      if [ -h $newname ]
       then
-        rm "${newname}"
+        rm ${newname}
       fi
-        ln -s "${tarname} ${newname}"
+        ln -s ${tarname} ${newname}
     fi
     
-    if [ -f "${project}-testimages.tar.gz" ]
+    if [ -f ${project}-testimages.tar.gz ]
     then
-      if [ ! -h  "${deb_name}_${debianversion}.orig-testimages.tar.gz" ]
+      if [ ! -h  ${deb_name}_${debianversion}.orig-testimages.tar.gz ]
       then
-        ln -s "${project}-testimages.tar.gz ${deb_name}_${debianversion}.orig-testimages.tar.gz"
+        ln -s ${project}-testimages.tar.gz ${deb_name}_${debianversion}.orig-testimages.tar.gz
       fi
     fi
     
-    cd "${directory}"
-    cp -r "${project_directory}/package/${target_system}" debian
-    cp "${project_directory}/copyright debian"
+    cd ${directory}
+    cp -r ${project_directory}/package/${target_system} debian
+    cp ${project_directory}/copyright debian
     
     #handle test images
-    if [ -f "../${deb_name}_${debianversion}.orig-testimages.tar.gz" ]
+    if [ -f ../${deb_name}_${debianversion}.orig-testimages.tar.gz ]
     then
       if [ ! -d testimages ]
       then
         mkdir testimages
       fi
       cd testimages
-      tar -xzf  "../${deb_name}_${debianversion}.orig-testimages.tar.gz"
+      tar -xzf  ../${deb_name}_${debianversion}.orig-testimages.tar.gz
       cd ..
     else
       # Disable to skip tests during build
@@ -220,7 +217,7 @@ build_deb() {
       #export DEB_BUILD_OPTIONS=nocheck
     fi
 
-    case "$debian_version" in
+    case $debian_version in
         9)
             debian_name=stretch
             ;;
@@ -232,20 +229,21 @@ build_deb() {
             ;;
     esac
 
-    dch --force-distribution  -v "${debianversion}-1 upstream development build of ${project} ${version}"
-    dch --force-distribution -D "${debian_name}-backports" -l~bpo${debian_version}+ "${project} snapshot ${version} built for ${target_system}"
+    dch --force-distribution  -v ${debianversion}-1 "upstream development build of ${project} ${version}"
+    dch --force-distribution -D ${debian_name}-backports -l~bpo${debian_version}+ "${project} snapshot ${version} built for ${target_system}"
+    #dch --bpo "${project} snapshot ${version} built for ${target_system}"
     dpkg-buildpackage -r
-    rc="$?"
+    rc=$?
     
-    if [ "$rc" -eq 0 ]; then
+    if [ $rc -eq 0 ]; then
       # move packages to dist directory
       echo Build succeeded...
-      rm -rf "${dist_directory}"
-      mkdir -p "${dist_directory}"
-      mv "${build_directory}/*.deb ${dist_directory}"
-      mv "${build_directory}/*.x* ${dist_directory}"
-      mv "${build_directory}/*.dsc ${dist_directory}"
-      mv "${build_directory}/*.changes ${dist_directory}"
+      rm -rf ${dist_directory}
+      mkdir -p ${dist_directory}
+      mv ${build_directory}/*.deb ${dist_directory}
+      mv ${build_directory}/*.x* ${dist_directory}
+      mv ${build_directory}/*.dsc ${dist_directory}
+      mv ${build_directory}/*.changes ${dist_directory}
       cd ../../..
     else
       echo Build failed, please investigate ...
@@ -256,44 +254,44 @@ build_deb() {
 
 build_stdeb () {
     echo "Build for debian using stdeb"
-    tarname="${project}-${strictversion}.tar.gz"
+    tarname=${project}-${strictversion}.tar.gz
     clean_up
 
     python3 setup.py sdist
-    cp -f "dist/${tarname} ${build_directory}"
-    cd "${build_directory}"
-    tar -xzf "${tarname}"
-    cd "${project}-${strictversion}"
+    cp -f dist/${tarname} ${build_directory}
+    cd ${build_directory}
+    tar -xzf ${tarname}
+    cd ${project}-${strictversion}
 
-    if [ "$stdeb_all_python" -eq 1 ]; then
+    if [ $stdeb_all_python -eq 1 ]; then
       echo Using Python 2+3
       python3 setup.py --command-packages=stdeb.command sdist_dsc --with-python2=True --with-python3=True --no-python3-scripts=True build --no-cython bdist_deb
-      rc="$?"
+      rc=$?
     else
       echo Using Python 3
       # bdist_deb feed /usr/bin using setup.py entry-points
       python3 setup.py --command-packages=stdeb.command build --no-cython bdist_deb
-      rc="$?"
+      rc=$?
     fi
 
     # move packages to dist directory
-    rm -rf "${dist_directory}"
-    mkdir -p "${dist_directory}"
-    mv -f deb_dist/*.deb "${dist_directory}"
+    rm -rf ${dist_directory}
+    mkdir -p ${dist_directory}
+    mv -f deb_dist/*.deb ${dist_directory}
 
     # back to the root
     cd ../../..
 }
 
 
-if [ "$use_stdeb" -eq 1 ]; then
+if [ $use_stdeb -eq 1 ]; then
     build_stdeb
 else
     build_deb
 fi
 
 
-if [ "$install" -eq 1 ]; then
+if [ $install -eq 1 ]; then
   sudo -v su -c  "dpkg -i ${dist_directory}/*.deb"
 fi
 
