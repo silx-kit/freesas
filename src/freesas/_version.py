8ce34c3a146041eca54cb7fd94097030b017babe
Jerome Kieffer <jerome.kieffer@esrf.fr>
2023-11-27 16:15:22 +0100

    new meson.build file

--- src/freesas/_version.py
+++ src/freesas/_version.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2015-2021 European Synchrotron Radiation Facility
+# Copyright (c) 2015-2023 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -52,7 +52,7 @@ Thus 2.1.0a3 is hexversion 0x020100a3.
 __authors__ = ["Jérôme Kieffer"]
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__date__ = "25/01/2021"
+__date__ = "27/11/2023"
 __status__ = "production"
 __docformat__ = 'restructuredtext'
 __all__ = ["date", "version_info", "strictversion", "hexversion", "debianversion",
@@ -70,7 +70,7 @@ PRERELEASE_NORMALIZED_NAME = {"dev": "a",
                               "candidate": "rc"}
 MAJOR = 0
 MINOR = 9
-MICRO = 2
+MICRO = 9
 RELEV = "dev"  # <16
 SERIAL = 0  # <16
 
